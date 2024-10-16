13f992c0a142b507dcc4ef3569314e046428d7e8
Alex <alex_ortega_98@hotmail.com>
2024-10-16 11:43:06 +0200

    Prueba 3 en dos partes

--- frontend/src/views/TestDetail.vue
+++ frontend/src/views/TestDetail.vue
@@ -5,30 +5,49 @@
       <v-card-text>
         <p>{{ test.description }}</p>
 
-        <!-- Campo para ingresar el código -->
+        <!-- Campo para ingresar el primer código -->
         <v-text-field
-            v-model="inputCode"
-            label="Introduce el código para desbloquear el audio"
-            :disabled="codeVerified"
+            v-model="inputCode1"
+            label="Introduce el primer código"
+            :disabled="code1Verified"
             required
         ></v-text-field>
         <v-btn
-            @click="verifyCode"
+            @click="verifyCode1"
             color="primary"
-            v-if="!codeVerified"
-        >Verificar Código</v-btn>
+            v-if="!code1Verified"
+        >
+          Verificar Primer Código
+        </v-btn>
 
-        <!-- Mensaje de error si el código es incorrecto -->
-        <p v-if="codeError" style="color: red;">Código incorrecto. Inténtalo de nuevo.</p>
+        <!-- Mensaje de error si el primer código es incorrecto -->
+        <p v-if="code1Error" style="color: red;">Código incorrecto. Inténtalo de nuevo.</p>
+        <!-- Campo para ingresar el segundo código -->
+        <v-text-field
+            v-if="test.id === 3 && code1Verified"
+            v-model="inputCode2"
+            label="Introduce el segundo código"
+            :disabled="code2Verified"
+            required
+        ></v-text-field>
+        <v-btn
+            v-if="test.id === 3 && code1Verified && !code2Verified"
+            @click="verifyCode2"
+            color="primary"
+        >
+          Verificar Segundo Código
+        </v-btn>
+        <!-- Mensaje de error para el segundo código -->
+        <p v-if="code2Error" style="color: red;">Segundo código incorrecto. Inténtalo de nuevo.</p>
 
         <!-- Reproductor de audio si el código es correcto -->
-        <audio v-if="codeVerified" :src="audioSrc" controls autoplay></audio>
+        <audio v-if="isAudioUnlocked" :src="audioSrc" controls autoplay></audio>
       </v-card-text>
     </v-card>
 
     <!-- Botón para finalizar la prueba una vez se escuche el audio -->
     <v-btn
-        v-if="codeVerified && audioFinished"
+        v-if="isAudioUnlocked"
         color="success"
         @click="finishTest"
     >Finalizar Prueba</v-btn>
@@ -41,22 +60,25 @@ export default {
   data() {
     return {
       test: {},
-      inputCode: '',  // Código introducido por el usuario
-      codeVerified: false,  // Si el código ha sido verificado correctamente
-      codeError: false,  // Para mostrar un mensaje de error si el código es incorrecto
+      inputCode1: '',  // Código introducido por el usuario para la primera etapa
+      inputCode2: '',  // Código introducido por el usuario para la segunda etapa
+      code1Verified: false,  // Si el primer código ha sido verificado correctamente
+      code2Verified: false,  // Si el segundo código ha sido verificado correctamente
+      code1Error: false,  // Para mostrar un mensaje de error si el primer código es incorrecto
+      code2Error: false,  // Para mostrar un mensaje de error si el segundo código es incorrecto
       audioSrc: '',  // Fuente del archivo de audio
       audioFinished: false,  // Para saber si el audio ha terminado de reproducirse
       tests: [
-        { id: 1, name: 'CIRCO DEL SOL', description: 'Descripción de la prueba 1', audio: require('@/assets/audios/prueba1.mp3'), code: '1111' },
-        { id: 2, name: 'ESCAPISMO', description: 'Descripción de la prueba 2', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 3, name: 'HOMBRE BALA', description: 'Descripción de la prueba 3', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 4, name: 'ADIVINACIÓN', description: 'Descripción de la prueba 4', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 5, name: 'TELEQUINESIS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 6, name: 'TELEQUINESIS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 7, name: 'ESPEJISMOS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 8, name: 'ILUSIONISMO', description: 'Tenéis que traer una cinta de tela de las que hay en la Morala, y llevársela al domador, que está dando vueltas por el pueblo con una vela. Él os dará lo que necesitáis.', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 9, name: 'EQUILIBRISMO', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
-        { id: 10, name:'MALABARISMO', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code: '2222' },
+        { id: 1, name: 'CIRCO DEL SOL', description: 'Descripción de la prueba 1', audio: require('@/assets/audios/prueba1.mp3'), code1: '1111',code2: null },
+        { id: 2, name: 'ESCAPISMO', description: 'Descripción de la prueba 2', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222' ,code2: null},
+        { id: 3, name: 'HOMBRE BALA', description: 'Descripción de la prueba 3', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: '3333' },
+        { id: 4, name: 'ADIVINACIÓN', description: 'Descripción de la prueba 4', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 5, name: 'TELEQUINESIS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 6, name: 'TELEQUINESIS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 7, name: 'ESPEJISMOS', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 8, name: 'ILUSIONISMO', description: 'Tenéis que traer una cinta de tela de las que hay en la Morala, y llevársela al domador, que está dando vueltas por el pueblo con una vela. Él os dará lo que necesitáis.', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 9, name: 'EQUILIBRISMO', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
+        { id: 10, name:'MALABARISMO', description: 'Descripción de la prueba 5', audio: require('@/assets/audios/prueba2.mp3'), code1: '2222',code2: null },
       ]
     };
   },
@@ -66,14 +88,32 @@ export default {
     this.test = this.tests.find(t => t.id === testId);
     this.audioSrc = this.test.audio;  // Asigna el audio correspondiente a la prueba
   },
+  computed: {
+    isAudioUnlocked() {
+      // El audio se desbloquea si:
+      // - En la prueba 3: ambos códigos son correctos
+      // - En las demás pruebas: solo el primer código es necesario
+      return this.test.id === 3
+          ? this.code1Verified && this.code2Verified
+          : this.code1Verified;
+    }
+  },
   methods: {
-    verifyCode() {
-      if (this.inputCode === this.test.code) {
-        this.codeVerified = true;
-        this.codeError = false;
-        this.audioFinished = true;
+    verifyCode1() {
+      if (this.inputCode1 === this.test.code1) {
+        this.code1Verified = true;
+        this.code1Error = false;
       } else {
-        this.codeError = true;
+        this.code1Error = true;
+      }
+    },
+    verifyCode2() {
+      if (this.inputCode2 === this.test.code2) {
+        this.code2Verified = true;
+        this.code2Error = false;
+        this.audioFinished = true;  // Permitir finalizar la prueba
+      } else {
+        this.code2Error = true;
       }
     },
     finishTest() {
